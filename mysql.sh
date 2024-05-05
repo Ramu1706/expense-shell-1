ca126fb11316e5e988928920ccfad767db55f98c
Ramu1706 <ram1706.devil@gmail.com>
2024-05-05 13:30:01 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -8,16 +8,20 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
 
 #Below code will be useful for idempotent nature
 mysql -h db.dawsproject.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
